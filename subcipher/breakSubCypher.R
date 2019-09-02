fb18ed547608c7b96800df536e2afc6b4d7836c6
alwinhughes <alwin.hughes@btopenworld.com>
2019-09-02 11:57:41 +0100

    added .gitignore

--- subcipher/breakSubCypher.R
+++ subcipher/breakSubCypher.R
@@ -32,118 +32,6 @@ getFrequenciesNum <- function(nummessage, alphabet) {
   return(M)
 }
 
-getBeta = function(message.num, alphabet) {
-  beta = numeric(length(alphabet))
-
-  for(i in 1:length(message.num)) {
-    if(is.na(message.num[i])) {
-      print(message.num[i])
-    }
-    beta[message.num[i]] = beta[message.num[i]] + 1
-  }
-
-
-  beta = mapply(function(x) {
-    if(x == 0) {
-      return(-14)
-    }
-    return(log(x/length(message.num)))
-  }, beta )
-
-  return(beta)
-}
-
-getP.log.frombook = function(book, alph) {
-  m <- matrix(0, nrow=length(alph), ncol=length(alph))
-  rownames(m) <- alph
-  colnames(m) <- alph
-  for (line in readLines(book, encoding="UTF-8")) {
-    if (line=="" | line == " " | line == "\r") next
-    line <- trim.line(line, alph)
-    m["\n", substr(line, 1, 1)] <- m["\n", substr(line, 1, 1)] + 1
-    for (i in 1:(nchar(line)-1)) {
-      current <- substr(line, i, i)
-      then <- substr(line, i+1, i+1)
-      if (!then %in% alph) print(then)
-      m[current, then] <- m[current, then] + 1
-    }
-    m[substr(line, nchar(line), nchar(line)), "\n"] <- m[substr(line, nchar(line), nchar(line)), "\n"] + 1
-  }
-  m = m/rowSums(m)
-  m = log(m)
-  m[m==-Inf] = -12
-  m[m==NaN]= -12
-  return(m)
-
-}
-
-getB.log.frombook = function(book, alph) {
-
-  m <- matrix(0, nrow=length(alph), ncol=length(alph))
-  rownames(m) <- alph
-  colnames(m) <- alph
-  for (line in readLines(book, encoding="UTF-8")) {
-    if (line=="" | line == " " | line == "\r") next
-    line <- trim.line(line, alph)
-    m["\n", substr(line, 1, 1)] <- m["\n", substr(line, 1, 1)] + 1
-    for (i in 1:(nchar(line)-1)) {
-      current <- substr(line, i, i)
-      then <- substr(line, i+1, i+1)
-      if (!then %in% alph) print(then)
-      m[current, then] <- m[current, then] + 1
-    }
-    m[substr(line, nchar(line), nchar(line)), "\n"] <- m[substr(line, nchar(line), nchar(line)), "\n"] + 1
-  }
-
-  return(log(rowSums(m)/ sum(m)))
-}
-
-
-trim.line <- function(line, alph) {
-  while (nchar(line)>0 & !(substr(line, 1, 1) %in% alph)) {
-    line <- substr(line, 2, nchar(line))
-  }
-  while (nchar(line)>0 & !(substr(line, nchar(line), nchar(line)) %in% alph)) {
-    line <- substr(line, 1, nchar(line)-1)
-  }
-  return(line)
-}
-
-
-
-getP.log = function(book.num, alphabet) {
-  P.log = matrix(ncol = length(alphabet), nrow =length(alphabet), 0)
-  
-
-  for(i in 2:length(book.num)) {
-    P.log[book.num[i-1], book.num[i]] = P.log[book.num[i-1], book.num[i]] +1
-  }
-
-
-
-  P.log = apply(P.log, 1, function(x) {
-    if(sum(x) == 0) { 
-      print("all zero")
-      return(numeric(length(alphabet)))
-    }
-    return(x/sum(x))
-  })
-
-
-
-  
-  P.log = apply(P.log, c(1,2), function(x) {
-    if(x == 0) {
-      return(-12)
-    }
-    return(log(x))
-  })
-
-
-  return(P.log)
-}
-
-
 transposition <- function(sigma, ij=NA) {
   if (sum(is.na(ij))>0) ij <- sample(sigma, 2)
   i <- min(ij)
@@ -229,7 +117,6 @@ breakCipher = function(ciphertext.num, beta, P, alphabet, M = 100) {
   print(accepts)
   return(pq)
 }
-
 
 constructMphi = function(msig, ij) {
   matphi = msig
